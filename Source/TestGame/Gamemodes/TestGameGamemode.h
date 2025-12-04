6b844388350b36244f9149996a4e9196b5d2c314
DESKTOP-J08DHS0\denng <denngerz@gmail.com>
2025-12-04 12:52:14 +0200

    Added GAS and mesh for character.

---
+++ Source/TestGame/Gamemodes/TestGameGamemode.h
@@ -0,0 +1,13 @@
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TestGameGamemode.generated.h"
+
+UCLASS()
+class TESTGAME_API ATestGameGamemode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
